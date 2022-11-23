83e9b6750ae277963a6167094be8ed1c08438920
DavyKing <yungdynamic53@gmail.com>
2022-11-23 13:26:20 +0100

    using shemir sharing for import

---
+++ lib/screens/import_with_secret_share.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ImportWithSecretShare extends StatefulWidget {
+  const ImportWithSecretShare({Key key}) : super(key: key);
+
+  @override
+  State<ImportWithSecretShare> createState() => _ImportWithSecretShareState();
+}
+
+class _ImportWithSecretShareState extends State<ImportWithSecretShare> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
