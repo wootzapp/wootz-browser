f27af0ff7b42e35d95f543db574aef64ee95f975
Aankirz <sahuankit453@gmail.com>
2024-04-02 19:18:53 +0000

    build fix

--- src/chrome/browser/ui/webui/hello_wallet/hello_wallet_page_handler.cc
+++ src/chrome/browser/ui/webui/hello_wallet/hello_wallet_page_handler.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/solana_wallet/page_handler/wallet_page_handler.h"
+#include "chrome/browser/ui/webui/hello_wallet/hello_wallet_page_handler.h"
 
 #include <utility>
 
@@ -11,11 +11,9 @@
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_finder.h"
 
-using segmentation_platform::proto::SegmentId;
-
 HelloWalletPageHandler::HelloWalletPageHandler(
-    mojo::PendingReceiver<segmentation_internals::mojom::PageHandler> receiver,
-    mojo::PendingRemote<segmentation_internals::mojom::Page> page,
+    mojo::PendingReceiver<solana_wallet::mojom::PageHandler> receiver,
+    mojo::PendingRemote<solana_wallet::mojom::Page> page,
     Profile* profile)
     : profile_(profile),
       receiver_(this, std::move(receiver)),
@@ -24,70 +22,7 @@ HelloWalletPageHandler::HelloWalletPageHandler(
 HelloWalletPageHandler::~HelloWalletPageHandler() = default;
 
 void HelloWalletPageHandler::ShowApprovePanelUI() {
-#if defined(TOOLKIT_VIEWS)
-  Browser* browser = chrome::FindBrowserWithProfile(profile_);
-  if (browser) {
-    chrome::ShowApproveWalletBubble(browser);
-  }
-#endif
+  
 }
 
-void SegmentationInternalsPageHandlerImpl::ExecuteModel(int segment_id) {
-  if (!service_proxy_) {
-    return;
-  }
-  service_proxy_->ExecuteModel(static_cast<SegmentId>(segment_id));
-}
-
-void SegmentationInternalsPageHandlerImpl::OverwriteResult(int segment_id,
-                                                           float result) {
-  if (!service_proxy_) {
-    return;
-  }
-  service_proxy_->OverwriteResult(static_cast<SegmentId>(segment_id), result);
-}
-
-void SegmentationInternalsPageHandlerImpl::SetSelected(
-    const std::string& segmentation_key,
-    int segment_id) {
-  if (!service_proxy_) {
-    return;
-  }
-
-  service_proxy_->SetSelectedSegment(segmentation_key,
-                                     static_cast<SegmentId>(segment_id));
-}
-
-void SegmentationInternalsPageHandlerImpl::OnServiceStatusChanged(
-    bool is_initialized,
-    int status_flag) {
-  page_->OnServiceStatusChanged(is_initialized, status_flag);
-}
-
-void SegmentationInternalsPageHandlerImpl::OnClientInfoAvailable(
-    const std::vector<segmentation_platform::ServiceProxy::ClientInfo>&
-        client_info) {
-  std::vector<segmentation_internals::mojom::ClientInfoPtr> available_clients;
-  for (const auto& info : client_info) {
-    auto client = segmentation_internals::mojom::ClientInfo::New();
-    client->segmentation_key = info.segmentation_key;
-    if (info.selected_segment) {
-      client->selected_segment =
-          segmentation_platform::proto::SegmentId_Name(*info.selected_segment);
-    } else {
-      client->selected_segment = "Not Ready";
-    }
-    for (const auto& status : info.segment_status) {
-      auto segment_data = segmentation_internals::mojom::SegmentInfo::New();
-      segment_data->segment_name =
-          segmentation_platform::proto::SegmentId_Name(status.segment_id);
-      segment_data->segment_id = status.segment_id;
-      segment_data->segment_data = status.segment_metadata;
-      segment_data->prediction_result = status.prediction_result;
-      segment_data->can_execute_segment = status.can_execute_segment;
-      client->segment_info.emplace_back(std::move(segment_data));
-    }
-    available_clients.emplace_back(std::move(client));
-  }
-  page_->OnClientInfoAvailable(std::move(available_clients));
-}
+void HelloWalletPageHandler::ShowWalletBackupUI() {}
