12f68266c08b9c17bc323c8173de3b9eae1ff249
Ankit Kiran <95562947+Aankirz@users.noreply.github.com>
2024-05-05 22:00:37 +0530

    Throttling init (#82)
    * Remove all files for browser code
    
    * Include src directory in wootz-browser
    
    * added gclient config
    
    * solana_wallet rust init
    
    * readme added
    
    * minor img fix
    
    * minor logo fix
    
    * Create README.md
    
    * branding fix
    
    * solana_wallet component special url ui init and  rust cxx added
    
    * solana_wallet component special url ui init and  rust cxx added
    
    * build fix
    
    * minor fix
    
    * throttling added
    
    * network throttling fix
    
    * chrome://throttle added
    
    * throttle minor fix
    
    * throttling fixed
    
    * throttling style changes

--- src/chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ src/chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -51,6 +51,7 @@
 #include "chrome/browser/ui/webui/metrics_internals/metrics_internals_ui.h"
 #include "chrome/browser/ui/webui/net_export_ui.h"
 #include "chrome/browser/ui/webui/net_internals/net_internals_ui.h"
+#include "chrome/browser/ui/webui/throttle/throttle_ui.h"
 #include "chrome/browser/ui/webui/ntp_tiles_internals_ui.h"
 #include "chrome/browser/ui/webui/omnibox/omnibox_ui.h"
 #include "chrome/browser/ui/webui/policy/policy_ui.h"
@@ -475,6 +476,8 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<NetExportUI>;
   if (url.host_piece() == chrome::kChromeUINetInternalsHost)
     return &NewWebUI<NetInternalsUI>;
+  if (url.host_piece() == chrome::kChromeUIThrottleHost)
+    return &NewWebUI<ThrottleUI>;
   if (url.host_piece() == chrome::kChromeUINTPTilesInternalsHost)
     return &NewWebUI<NTPTilesInternalsUI>;
   if (url.host_piece() == chrome::kChromeUIOmniboxHost)
