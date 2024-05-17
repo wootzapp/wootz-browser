104e9cd927fbc3c11e55018aa6cc0b5f8ca0734c
Ankit Kiran <95562947+Aankirz@users.noreply.github.com>
2024-05-17 19:06:54 +0530

    Wootzapp build fix (#85)
    * wootzapp build fix
    
    * wootzapp build fix
    
    * Update README.md
    
    * Apply .gitignore
    
    * Update README.md
    
    * minor fix
    
    * minor fix
    
    * Update README.md

--- src/chrome/browser/ui/views/global_media_controls/cast_device_selector_view_unittest.cc
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "chrome/browser/ui/views/global_media_controls/cast_device_selector_view.h"
-
-// TODO(yrw): Add unit tests when CastDeviceSelectorView is implemented.
--- src/third_party/blink/web_tests/external/wpt/webnn/validation_tests/resample2d.https.any.js
+++
@@ -1,8 +0,0 @@
-// META: title=validation tests for WebNN API resample2d operation
-// META: global=window,dedicatedworker
-// META: script=../resources/utils_validation.js
-// META: timeout=long
-
-'use strict';
-
-validateOptionsAxes('resample2d', 4);
