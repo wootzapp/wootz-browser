107a2c464e2012f13a52564a2405142e5533a64a
Ankit Kiran <95562947+Aankirz@users.noreply.github.com>
2024-05-17 19:06:54 +0530

    Wootzapp build fix (#85)
    * wootzapp build fix
    
    * wootzapp build fix
    
    * Update README.md
    
    * Apply .gitignore
    
    * Update README.md
    
    * minor fix
    
    * minor fix
    
    * Update README.md

---
+++ chrome/browser/resources/wootzapp_throttle/wootzapp_index.js
@@ -0,0 +1,9 @@
+// Copyright 2024 The WootzApp Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {MainView} from './wootzapp_.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  MainView.getInstance();  // from main.js
+});
