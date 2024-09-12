230d41cf5dfc75b34165cde6ca6bbae407408dbe
devjangid15 <jangiddev195@gmail.com>
2024-09-12 13:50:50 +0200

    Artifact's integration with ThirdWeb Wallet

---
+++ src/chrome/browser/resources/rewards/dist/static/js/69701.dab9e287.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrewards=self.webpackChunkrewards||[]).push([[69701],{69701:(l,n,e)=>{e.r(n),e.d(n,{wallet:()=>a});const a={id:"xyz.orion",name:"Orion",homepage:"https://orion.xyz",image_id:"85007828-9522-4517-6a55-6f376de8a500",app:{browser:null,ios:"https://itunes.apple.com/app/id1659758160",android:null,mac:null,windows:null,linux:null,chrome:null,firefox:null,safari:null,edge:null,opera:null},rdns:null,mobile:{native:"orion://wc?",universal:null},desktop:{native:null,universal:null}}}}]);
+//# sourceMappingURL=69701.dab9e287.chunk.js.map
