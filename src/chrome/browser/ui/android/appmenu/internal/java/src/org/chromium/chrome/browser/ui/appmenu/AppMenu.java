eafa3b0bbcd1073209eea07d0b462db8fa86fa37
kritagya-khanna <kritagykhanna21k@gmail.com>
2025-02-01 11:38:31 +0100

    fixed latest issues

--- src/chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenu.java
+++ src/chrome/browser/ui/android/appmenu/internal/java/src/org/chromium/chrome/browser/ui/appmenu/AppMenu.java
@@ -458,10 +458,11 @@ public class AppMenu extends BottomSheetDialogFragment
         HorizontalScrollView scrollView = view.findViewById(R.id.extensions_scroll_view);
         LinearLayout parent = view.findViewById(R.id.app_menu_extensions);
 
-        // Check if we're in incognito mode
-        if (mHandler != null && mHandler.getActivityTab() != null &&
-                mHandler.getActivityTab().isIncognito()) {
-            // Hide all extension-related views in incognito mode
+        if (mHandler != null && 
+            (mHandler.getActivityTab() == null || // Tab switcher case
+             mHandler.getActivityTab().isIncognito() || // Incognito case
+               mHandler.getActivityTab().isCustomTab())) { //For custom tabs
+            // Hide all extension-related views
             extensionsDivider.setVisibility(View.GONE);
             scrollView.setVisibility(View.GONE);
             parent.setVisibility(View.GONE);
