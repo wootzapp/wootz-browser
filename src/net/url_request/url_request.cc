8a1d0420004e4eaeffc747b7fb068a04c297684b
kritagya-khanna <kritagykhanna21k@gmail.com>
2025-02-05 08:25:53 +0100

    url to regular expression

--- src/net/url_request/url_request.cc
+++ src/net/url_request/url_request.cc
@@ -5,6 +5,7 @@
 #include "net/url_request/url_request.h"
 
 #include <utility>
+#include <regex>
 
 #include "base/compiler_specific.h"
 #include "base/functional/bind.h"
@@ -540,13 +541,13 @@ void URLRequest::set_allow_credentials(bool allow_credentials) {
 
 void URLRequest::Start() {
   DCHECK(delegate_);
-  LOG(INFO) << "kritagya URLRequest::Start()" << url().spec();
-  if (url().spec() == "https://s.imgur.com/desktop-assets/js/footercta.3442ab27b52a22da9ab2.bundle.js") {
-
-    status_ = ERR_BLOCKED_BY_CLIENT;
-
-    return;
-  }
+  LOG(INFO) << "kritagya URLRequest::Start() " << url().spec();
+  std::regex url_pattern("https://s\\.imgur\\.com/desktop-assets/js/footercta\\..*\\.bundle\\.js");
+    // Check if the URL matches the pattern
+    if (std::regex_match(url().spec(), url_pattern)) {
+        // Block the request
+        return;
+    }
   if (status_ != OK)
     return;
 
