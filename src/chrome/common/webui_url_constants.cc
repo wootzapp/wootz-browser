7161ba3f84f57fb41e63b11bc99d4e56801c82a7
1311-hack1 <sagarexternaluse@gmail.com>
2025-02-15 07:08:24 +0100

    extension store URL constants

--- src/chrome/common/webui_url_constants.cc
+++ src/chrome/common/webui_url_constants.cc
@@ -28,6 +28,8 @@ namespace chrome {
 
 const char kChromeUIAboutHost[] = "about";
 const char kChromeUIAboutURL[] = "wootzapp://about/";
+const char kChromeUIExtensionStoreURL[] = "wootzapp://extension-store/";
+const char kChromeUIExtensionStoreHost[] = "extension-store";
 const char kChromeUIActivateSafetyCheckSettingsURL[] =
     "wootzapp://settings/safetyCheck?activateSafetyCheck";
 const char kChromeUIAccessibilityHost[] = "accessibility";
