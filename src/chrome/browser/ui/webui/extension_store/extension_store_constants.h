1f9a3c968ead4036f2f8480d3ff553026bcd3fa0
1311-hack1 <sagarexternaluse@gmail.com>
2025-05-22 13:58:52 +0200

    moving flow store URLs to constants file

---
+++ src/chrome/browser/ui/webui/extension_store/extension_store_constants.h
@@ -0,0 +1,15 @@
+// Copyright 2025 The Wootzapp Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_EXTENSION_STORE_EXTENSION_STORE_CONSTANTS_H_
+#define CHROME_BROWSER_UI_WEBUI_EXTENSION_STORE_EXTENSION_STORE_CONSTANTS_H_
+
+namespace extension_store {
+
+// Base URL for fetching the extensions list.
+extern const char kExtensionStoreBaseUrl[];
+
+}  // namespace extension_store
+
+#endif  // CHROME_BROWSER_UI_WEBUI_EXTENSION_STORE_EXTENSION_STORE_CONSTANTS_H_
